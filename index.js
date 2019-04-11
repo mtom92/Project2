4a4d03e1f156cd7ed1ceabed0529192eea7669c2
mtom92 <torresmoranm@gmail.com>
2019-04-11 11:48:25 -0700

    home page working

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 //include .env variables
+require('dotenv').config;
 
 //require necessary modules
-
+let express = require('express');
 //declare express app
-
+let app = express();
 //set view engine
-
+app.set('view engine', 'ejs');
 //include (use) middle ware
 
 //include routes from controllers
 
-//make a home route get /
-
+//make a home route GET /
+app.get('/', (req,res)=>{
+  res.send('STUB')
+});
 //listen from your port
+app.listen(process.env.PORT || 3000);
