876d6238d2682fb3cc7dcbfe47a4b5aad4a640dd
mtom92 <torresmoranm@gmail.com>
2019-04-11 11:38:45 -0700

    added readme.md stub

---
+++ index.js
@@ -0,0 +1,15 @@
+//include .env variables
+
+//require necessary modules
+
+//declare express app
+
+//set view engine
+
+//include (use) middle ware
+
+//include routes from controllers
+
+//make a home route get /
+
+//listen from your port
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Node/Express/PostgreSQL Boilerplate
+
+## Routes Table
+ TBD - table of available routes
+
+## User Model Set Up
+
+TBD - diagram of User table
+
+## Usage
+
+TBD - Directions 
