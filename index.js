dd9930219ce9413588dc3a04f3d239761408a7ad
mtom92 <torresmoranm@gmail.com>
2019-04-11 11:54:40 -0700

    home page rendered

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ require('dotenv').config;
 
 //require necessary modules
 let express = require('express');
+let layouts = require('express-ejs-layouts');
 //declare express app
 let app = express();
 //set view engine
 app.set('view engine', 'ejs');
 //include (use) middle ware
-
+app.use(layouts)
 //include routes from controllers
 
 //make a home route GET /
 app.get('/', (req,res)=>{
-  res.send('STUB')
+  res.render('home')
 });
 //listen from your port
 app.listen(process.env.PORT || 3000);
